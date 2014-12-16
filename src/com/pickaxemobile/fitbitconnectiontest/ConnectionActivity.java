73704708867f14d1dc62da3a3a428a41b7e2c87c
Anna Billstrom <banane@gmail.com>
2014-12-16 14:43:15 -0800

    working

--- src/com/pickaxemobile/fitbitconnectiontest/ConnectionActivity.java
+++ src/com/pickaxemobile/fitbitconnectiontest/ConnectionActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -45,8 +46,10 @@ public class ConnectionActivity extends Activity implements OAuthCallback {
 
     public void connect(View v){
         final OAuth oauth = new OAuth(this);
+       // Log.d(TAG, "pub key: " + MyFitbitConnectionTest.OAUTH_IO_PUBLIC_KEY);
         oauth.initialize(MyFitbitConnectionTest.OAUTH_IO_PUBLIC_KEY); //OAuth.io public key, not Fitbit's
-        oauth.popup("fitbit", ConnectionActivity.this); //TODO seems bad to return to main activity with callback
+        oauth.popup("fitbit", ConnectionActivity.this);
+        //TODO seems bad to return to main activity with callback
     }
 
     public void onFinished(OAuthData data) {
@@ -56,50 +59,29 @@ public class ConnectionActivity extends Activity implements OAuthCallback {
             nameTextView.setText("error, " + data.error);
         }
         Log.d(MyFitbitConnectionTest.TAG, "data provider: " + data.provider + " data http" + data.request.toString());
-        new GetProfileServerTask().execute(data);
+            // You can access the tokens through data.token and data.secret
 
 
+            // Let's skip the NetworkOnMainThreadException for the purpose of this sample.
+
+        //TODO definitely have to make this async
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
+
+            // To make an authenticated request, you can implement OAuthRequest with your preferred way.
+            // Here, we use an URLConnection (HttpURLConnection) but you can use any library.
+        getProfileData(data);
+        getGoalData(data);
     }
 
-    private class GetProfileServerTask extends AsyncTask<OAuthData, Void, String> {
-
-        protected String doInBackground(OAuthData... data) {
-            String resultString = profileHTTPGET(data.toString());
-            return resultString;
-        }
-
-        protected void onPreExecute() {
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            //TODO parse out result
-            /*try{
-                JSONObject petObj = new JSONObject(result);
-                //	public Pet(String name, String story, int type, int id){
-
-                // JSONObject petObj = jObject.getJSONObject("id");
-                int id = petObj.getInt("id");
-                String name = petObj.getString("name");
-
-
-
-            } catch (JSONException e){
-                Log.e(TAG, "exception: " + e.getMessage());
-            }*/
-            String resultString = "";
-
-        }
-    }
-
-    private void doProfileQuery(OAuthData data){
-        // this is from example, don't want to use this http library
+    private void getProfileData(OAuthData data){
         data.http(data.provider.equals("fitbit") ? "/1/user/-/profile.json" : "/1.1/account/verify_credentials.json", new OAuthRequest() {
             private URL url;
             private URLConnection con;
 
             @Override
             public void onSetURL(String _url) {
+                Log.d(MyFitbitConnectionTest.TAG, "onsetURL url:" + _url);
+
                 try {
                     url = new URL(_url);
                     con = url.openConnection();
@@ -113,18 +95,25 @@ public class ConnectionActivity extends Activity implements OAuthCallback {
 
             @Override
             public void onReady() {
+                StringBuilder total = new StringBuilder();
+                String line = "";
+                Log.d(MyFitbitConnectionTest.TAG, "onReady() back from call");
                 try {
                     BufferedReader r = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                    StringBuilder total = new StringBuilder();
-                    String line;
                     while ((line = r.readLine()) != null) {
                         total.append(line);
                     }
-                    Log.d("FITBIT", "query result: " + total);
+                    Log.d(MyFitbitConnectionTest.TAG, "onReady() total: " + total);
+                } catch (Exception e) { e.printStackTrace(); }
+
+                try {
                     JSONObject result = new JSONObject(total.toString());
                     JSONObject user = result.getJSONObject("user");
                     nameTextView.setText(user.getString("displayName"));
-                } catch (Exception e) { e.printStackTrace(); }
+
+                } catch (JSONException e){
+                    Log.e(MyFitbitConnectionTest.TAG, "json error: " + e.getLocalizedMessage());
+                }
             }
 
             @Override
@@ -134,31 +123,56 @@ public class ConnectionActivity extends Activity implements OAuthCallback {
         });
     }
 
-    public String profileHTTPGET(String URL) {
-        // this is the http library (apache) we want to use
-        Log.d(TAG,"get profile data,  url: " + URL);
-        HttpEntity resEntity;
-        String responseString = "";
+    private void getGoalData(OAuthData data){
+        data.http(data.provider.equals("fitbit") ? "/1/user/-/activities/goals/daily.json" : "/1.1/account/verify_credentials.json", new OAuthRequest() {
+            private URL url;
+            private URLConnection con;
 
-        try {
+            @Override
+            public void onSetURL(String _url) {
+                Log.d(MyFitbitConnectionTest.TAG, "onsetURL url:" + _url);
 
-            HttpUriRequest request = new HttpGet(URL);
-
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpResponse response = httpclient.execute(request);
-
-            resEntity = response.getEntity();
-            if (resEntity != null) {
-                responseString = EntityUtils.toString(resEntity);
-                //	Log.i(TAG,"get response: " + responseString);
-            } else {
-                Log.d(MyFitbitConnectionTest.TAG, "resEntity is null");
+                try {
+                    url = new URL(_url);
+                    con = url.openConnection();
+                } catch (Exception e) { e.printStackTrace(); }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-            Log.e(TAG, e.getMessage());
-        }
 
-        return responseString;
+            @Override
+            public void onSetHeader(String header, String value) {
+                con.addRequestProperty(header, value);
+            }
+
+            @Override
+            public void onReady() {
+                StringBuilder total = new StringBuilder();
+                String line = "";
+                Log.d(MyFitbitConnectionTest.TAG, "onReady() back from call");
+                try {
+                    BufferedReader r = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    while ((line = r.readLine()) != null) {
+                        total.append(line);
+                    }
+                    Log.d(MyFitbitConnectionTest.TAG, "onReady() total: " + total);
+                } catch (Exception e) { e.printStackTrace(); }
+
+                try {
+                    JSONObject result = new JSONObject(total.toString());
+                    JSONObject goals = result.getJSONObject("goals");
+                    stepGoalTextView.setText(goals.getString("steps"));
+
+                } catch (JSONException e){
+                    Log.e(MyFitbitConnectionTest.TAG, "json error: " + e.getLocalizedMessage());
+                }
+            }
+
+            @Override
+            public void onError(String message) {
+                nameTextView.setText("error: " + message);
+            }
+        });
     }
-}
+
+
+
+    }
--- src/com/pickaxemobile/fitbitconnectiontest/MyFitbitConnectionTest.java
+++ src/com/pickaxemobile/fitbitconnectiontest/MyFitbitConnectionTest.java
@@ -4,7 +4,7 @@ package com.pickaxemobile.fitbitconnectiontest;
  * Created by banane on 12/16/14.
  */
 public class MyFitbitConnectionTest {
-    public static final String OAUTH_IO_PUBLIC_KEY = "YOUR OAUTH.IO PUBLIC KEY HERE";
+    public static final String OAUTH_IO_PUBLIC_KEY = "[INPUT YOUR OAUTH.IO PUBLIC KEY HERE]";
 
     public static final String TAG = "FITBIT";
 }
