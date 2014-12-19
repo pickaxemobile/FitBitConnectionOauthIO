44eef1ba10e0dbc02d6ef70ddd20ba5a97e558de
Anna Billstrom <banane@gmail.com>
2014-12-19 10:25:41 -0800

    added OAuth.jar

--- src/com/pickaxemobile/fitbitconnectiontest/MyFitbitConnectionTest.java
+++ src/com/pickaxemobile/fitbitconnectiontest/MyFitbitConnectionTest.java
@@ -3,8 +3,8 @@ package com.pickaxemobile.fitbitconnectiontest;
 /**
  * Created by banane on 12/16/14.
  */
+
 public class MyFitbitConnectionTest {
     public static final String OAUTH_IO_PUBLIC_KEY = "[INPUT YOUR OAUTH.IO PUBLIC KEY HERE]";
-
     public static final String TAG = "FITBIT";
 }
